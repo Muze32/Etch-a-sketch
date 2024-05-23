4230af483713469b8f9dd2c47ea797a306f8e7d9
Muze <mark46716@gmail.com>
2024-05-23 14:55:33 -0300

    Add custom input for the size of grid
    Added an button which prompts the user for an
    input which is gonna be the size of the grid.
    Also, deletes any prior grid so that there is no
    conflict.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,27 @@
 const body = document.querySelector("body");
 const full_grid = document.querySelector(".full_grid");
+const input_button = document.querySelector(".input");
 
-for(let i = 0; i < 16; i++) {
-    const container = document.createElement("div");
-    container.classList.add("container");
-    for (let j = 0; j < 16; j++) {
-        const grid = document.createElement("div");
-        grid.setAttribute("class", "grid");
-        grid.textContent = "test";
-        container.appendChild(grid);
+input_button.addEventListener("click", () => {
+    let input =  prompt("Enter the size of the grid (Up to 100): ");
+
+    while (full_grid.firstChild) {
+        full_grid.removeChild(full_grid.firstChild);
     }
-    full_grid.appendChild(container);
-}
-
+    
+    for(let i = 0; i < input; i++) {
+        const container = document.createElement("div");
+        container.classList.add("container");
+        for (let j = 0; j < input; j++) 
+            {
+            const grid = document.createElement("div");
+            grid.setAttribute("class", "grid");
+            grid.textContent = "test";
+            container.appendChild(grid);
+        }
+        full_grid.appendChild(container);
+    }
+})
 
 full_grid.addEventListener("mouseover", (event) => {
     event.target.setAttribute("style", "background-color: black");
