77b2475639557dd9309f56d8f2967cd021d21f2a
Muze <mark46716@gmail.com>
2024-05-23 16:04:09 -0300

    Fix grid text and its size Previously, each grid had an text placeholder. Also, the total size of the grid changed according to the amount of squares.

--- scripts.js
+++ scripts.js
@@ -2,27 +2,31 @@ const body = document.querySelector("body");
 const full_grid = document.querySelector(".full_grid");
 const input_button = document.querySelector(".input");
 
-input_button.addEventListener("click", () => {
-    let input =  prompt("Enter the size of the grid (Up to 100): ");
+input_button.addEventListener("click", () => 
+{
+    let input =  prompt("Enter the size of the grid (Up to 100): ");    
 
-    while (full_grid.firstChild) {
+    while (full_grid.firstChild) 
+    {
         full_grid.removeChild(full_grid.firstChild);
     }
-    
-    for(let i = 0; i < input; i++) {
+
+    for(let i = 0; i < input; i++) 
+    {
         const container = document.createElement("div");
         container.classList.add("container");
         for (let j = 0; j < input; j++) 
-            {
+        {
             const grid = document.createElement("div");
             grid.setAttribute("class", "grid");
-            grid.textContent = "test";
             container.appendChild(grid);
         }
         full_grid.appendChild(container);
     }
 })
+console.log(full_grid.offsetWidth)
 
-full_grid.addEventListener("mouseover", (event) => {
+full_grid.addEventListener("mouseover", (event) => 
+{
     event.target.setAttribute("style", "background-color: black");
 })
