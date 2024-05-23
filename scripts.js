f1b669a9466544327891360c47c50c96bf2dab97
Muze <mark46716@gmail.com>
2024-05-23 14:26:03 -0300

    Make grid 16x16
    Made an 16x16 grids and appended it into a div
    (otherwise js wont be able to detect any event)

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+const full_grid = document.querySelector(".full_grid");
+
+for(let i = 0; i < 16; i++) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement("div");
+        grid.setAttribute("class", "grid");
+        grid.textContent = "test";
+        container.appendChild(grid);
+    }
+    full_grid.appendChild(container);
+}
+
