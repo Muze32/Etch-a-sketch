e8af9058d6a919d57f2b306ab6b379adcb732b97
Muze <mark46716@gmail.com>
2024-05-29 11:05:39 -0300

    Add initial grid of fixed size

--- scripts.js
+++ scripts.js
@@ -5,6 +5,34 @@ const rainbow = document.querySelector(".rainbow");
 const mono = document.querySelector(".mono");
 let random_color = false;
 
+function create_grid(input) {
+    if (input >= 0 && input <= 100) 
+        {
+            input_button.textContent = `Change size (${input} x ${input})`;
+            for(let i = 0; i < input; i++) 
+                {
+                    const container = document.createElement("div");
+                    container.classList.add("container");
+                    for (let j = 0; j < input; j++) 
+                    {
+                        const grid = document.createElement("div");
+                        grid.setAttribute("class", "grid");
+                        grid.style.height = "50px";
+                        grid.style.height = ((full_grid.offsetHeight / input) - 2).toString() + "px";
+                        grid.style.width = ((full_grid.offsetWidth / input) - 2).toString() + "px";
+                        container.appendChild(grid);
+                    }
+                    full_grid.appendChild(container);
+                }
+            
+        }
+        else {
+            prompt("Please enter an valid number!");
+        }
+    
+}
+create_grid(5);
+
 input_button.addEventListener("click", () => 
 {
     let input =  prompt("Enter the size of the grid (Up to 100): ");    
@@ -13,30 +41,7 @@ input_button.addEventListener("click", () =>
     {
         full_grid.removeChild(full_grid.firstChild);
     }
-    
-    if (input >= 0 && input <= 100) 
-    {
-        input_button.textContent = `Change size (${input} x ${input})`;
-        for(let i = 0; i < input; i++) 
-            {
-                const container = document.createElement("div");
-                container.classList.add("container");
-                for (let j = 0; j < input; j++) 
-                {
-                    const grid = document.createElement("div");
-                    grid.setAttribute("class", "grid");
-                    grid.style.height = "50px";
-                    grid.style.height = ((full_grid.offsetHeight / input) - 2).toString() + "px";
-                    grid.style.width = ((full_grid.offsetWidth / input) - 2).toString() + "px";
-                    container.appendChild(grid);
-                }
-                full_grid.appendChild(container);
-            }
-        
-    }
-    else {
-        prompt("Please enter an valid number!");
-    }
+    create_grid(input);
 })
 
 mono.addEventListener("click", () => {
@@ -53,7 +58,7 @@ full_grid.addEventListener("mouseover", (event) =>
 {
     if (event.target.className == "grid" && !random_color) 
     {
-        event.target.style["background-color"] = "black";
+        event.target.style["background-color"] = "red";
     }
     else if (event.target.className == "grid" && random_color) {
         event.target.style["background-color"] = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
